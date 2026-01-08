7fd52d8f76d787034d860aeb762692aef8dc6d11
NoahNelken <nnelken27@dths.org>
2026-01-07 16:14:51 -0800

    learn cuz cool
    ok

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,9 +6,6 @@ package frc.robot;
 
 import java.util.Timer;
 
-import org.littletonrobotics.junction.LoggedRobot;
-import org.littletonrobotics.junction.Logger;
-
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
@@ -16,7 +13,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-public class Robot extends LoggedRobot {
+public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private final RobotContainer m_robotContainer;
@@ -29,22 +26,6 @@ public class Robot extends LoggedRobot {
 
   @Override
   public void robotPeriodic() {
-    Logger.recordMetadata("ProjectName", "MyProject");
-
-    Logger.start();
-
-    Logger.recordOutput("RobotPose", new Pose2d());
-    Logger.recordOutput("ZeroedComponentPoses", new Pose3d[] {new Pose3d()});
-    Logger.recordOutput(
-      "FinalComponentPoses",
-      new Pose3d[] {
-        new Pose3d(
-          -0.238, 0.0, 0.289, new Rotation3d(0.0, Math.sin(edu.wpi.first.wpilibj.Timer.getTimestamp
-          ()) - 1.0, 0.0)
-        )
-      }
-    );
-
     CommandScheduler.getInstance().run(); 
     
   }
